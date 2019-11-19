445b2298ddaa93d492af9c1310dfccbfa7408c8f
am10 <adventam10@gmail.com>
2019-11-19 23:36:59 +0900

    Add 記事一覧テーブルセル

---
+++ SampleApp/SampleApp/日本語ソース/記事テーブルセル.swift
@@ -0,0 +1,14 @@
+//
+//  記事テーブルセル.swift
+//  SampleApp
+//
+//  Created by am10 on 2019/11/19.
+//  Copyright © 2019 am10. All rights reserved.
+//
+
+import UIKit
+
+final class 記事テーブルセル: テーブルセル {
+
+    @IBOutlet weak var タイトルラベル: ラベル!
+}
